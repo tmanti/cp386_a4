d906a301dc042f11e35943eb472725c57ae22ca3
LY0530 <109489102+LY0530@users.noreply.github.com>
2022-07-17 19:51:45 -0400

    Update 200188260_190555710_a4.c

--- 200188260_190555710_a4.c
+++ 200188260_190555710_a4.c
@@ -1,2 +1,17 @@
 //Thomas Ingram - github: tmanti
 //Lan You       - github: LY0530
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]) {
+    int* ptr;
+    
+    int size = atoi(argv[1]);
+    ptr = (int*)malloc(size*sizeof(int));
+    printf("Allocated %d bytes of memory\n",size);
+    
+    free(ptr);
+    return 0;
+}
