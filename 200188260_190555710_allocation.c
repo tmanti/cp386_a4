93af0fc381208a2bd7f9e2df8a51f1414cec121c
LY0530 <109489102+LY0530@users.noreply.github.com>
2022-07-17 20:08:07 -0400

    Update 200188260_190555710_allocation.c

--- 200188260_190555710_allocation.c
+++ 200188260_190555710_allocation.c
@@ -4,13 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     int* ptr;
+    char command[20];
     
     int size = atoi(argv[1]);
     ptr = (int*)malloc(size*sizeof(int));
     printf("Allocated %d bytes of memory\n",size);
+
+    while(scanf("%s",command)) {
+        if (strcmp(command,"Exit")==0) exit(0);
+        //loop
+    }
     
     free(ptr);
     return 0;
